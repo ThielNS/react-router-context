a23bb98aa11c28e66d9063b7fc162f1363394d7a
Thiel <gabrieln.souza@outlook.com>
2022-11-21 23:52:21 -0300

    - Changed Interface RouteProps to type RouteProps

--- src/types/Routing.ts
+++ src/types/Routing.ts
@@ -1,10 +1,10 @@
 import { RouteProps as RouteObj } from 'react-router-dom';
 
-export interface RouteProps<P = any> extends RouteObj {
+export type RouteProps<P = any> = RouteObj & {
   children?: RouteProps<P>[];
   roles?: string[];
   params?: P;
-}
+};
 
 export type RoutesProps = { routes?: RouteProps[] };
 
